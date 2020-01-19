3d057dffe54db3cc6f06ff60898874099071dc69
Adrian Sidor <adriansidor11@wp.pl>
2020-01-19 14:39:48 +0100

    Add test class

---
+++ src/test/java/ResponseServiceImplTest.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ResponseServiceImplTest {
+
+}
